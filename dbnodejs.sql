654dead2e4122ec6cd6097ae72cf8f1fc7cc8410
kennyincisowow <kennyinciso@upeu.edu.pe>
2021-12-21 14:15:03 -0500

    comiit 5

--- dbnodejs.sql
+++ dbnodejs.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-12-2021 a las 00:24:48
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.12
+-- Tiempo de generación: 21-12-2021 a las 20:12:40
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,42 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `dbnodejs`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actividades`
+--
+
+CREATE TABLE `actividades` (
+  `actividad_id` int(11) NOT NULL,
+  `actividad_nombre` varchar(50) NOT NULL,
+  `actividad_descricpion` varchar(50) NOT NULL,
+  `actividad_fecha` date NOT NULL,
+  `actividades_tipo` varchar(50) NOT NULL,
+  `actividad_sancion_importe` decimal(10,2) NOT NULL,
+  `actividad_estado` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `actividades`
+--
+
+INSERT INTO `actividades` (`actividad_id`, `actividad_nombre`, `actividad_descricpion`, `actividad_fecha`, `actividades_tipo`, `actividad_sancion_importe`, `actividad_estado`) VALUES
+(3, 'Barrer', 'asasa', '2021-12-19', 'l', '343.00', 'activo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asistencia`
+--
+
+CREATE TABLE `asistencia` (
+  `asistencia_id` int(11) NOT NULL,
+  `asistencia_estado` varchar(1) NOT NULL,
+  `asistencia_stand_id` int(11) NOT NULL,
+  `asistencia_actividad_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -41,69 +77,14 @@ CREATE TABLE `books` (
 
 INSERT INTO `books` (`id`, `name`, `author`, `created_at`, `updated_at`) VALUES
 (1, 'La ciudad y los perros', 'Mario Vargas Llosa', '2021-11-08 19:15:44', '2021-11-08 19:59:46'),
-(2, 'El caballero Carmelo', 'Abraham Valdelomar', '2021-11-08 19:59:34', '2021-11-08 19:59:34');
+(2, 'El caballero Carmeler', 'Abraham Valdelomar', '2021-11-08 19:59:34', '2021-12-16 04:33:10'),
+(4, ' manuel chaprodo', 'el cabellerito', '2021-12-16 19:48:06', '2021-12-16 19:48:06'),
+(5, 'marc sorcio', 'la laguna', '2021-12-16 19:48:06', '2021-12-16 19:48:06'),
+(6, 'leon sercei', 'el ultimo', '2021-12-16 19:48:06', '2021-12-16 19:48:06'),
+(7, 'tobias tubiel', 'el deslus', '2021-12-16 19:48:08', '2021-12-16 19:48:08');
 
 -- --------------------------------------------------------
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `persona`
---
-
-CREATE TABLE `persona` (
-  ` persona_id` int(11) NOT NULL,
-  `persona_nombres` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `persona_roles` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `persona_ap_materno` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `persona_ap_paterno` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `persona_estado_civil` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `persona_ocupacion_profesion` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `persona_dni` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `persona_direccion` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `persona_telefono` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Volcado de datos para la tabla `persona`
---
-
-insert into persona (persona_id,persona_nombres,persona_roles,persona_ap_materno,persona_ap_paterno,persona_estado_civil,persona_ocupacion_profesion,persona_fecha_nacimiento,persona_dni,persona_direccion,persona_telefono) values ('1','kenny','gerente','inciso','inciso','s','estudiante','','43788956','paltas con camotes','978451236');
-insert into persona (persona_id,persona_nombres,persona_roles,persona_ap_materno,persona_ap_paterno,persona_estado_civil,persona_ocupacion_profesion,persona_fecha_nacimiento,persona_dni,persona_direccion,persona_telefono) values ('2','jork','asistonto','cardios','zarse','s','vagancia','','41895623','paltas sin camotes','975482159');
-insert into persona (persona_id,persona_nombres,persona_roles,persona_ap_materno,persona_ap_paterno,persona_estado_civil,persona_ocupacion_profesion,persona_fecha_nacimiento,persona_dni,persona_direccion,persona_telefono) values ('3','krel','saladero','most','charls','s','amo de jato','','45796956','paltas con verdad','968475123');
-insert into persona (persona_id,persona_nombres,persona_roles,persona_ap_materno,persona_ap_paterno,persona_estado_civil,persona_ocupacion_profesion,persona_fecha_nacimiento,persona_dni,persona_direccion,persona_telefono) values ('4','crasl','subgerente','inciso','inciso','s','priocitas','curdate()-interval 500 day','47788956','volvandetas camotes','957153464');
-
-
--- --------------------------------------------------------
---
--- Estructura de tabla para la tabla `books`
---
-
-CREATE TABLE `socios` (
-  `socio_id` int(11) NOT NULL,
-  `socio_codigo` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `socio_persona_id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Volcado de datos para la tabla `books`
---
-
-insert into socios (socio_id,socio_codigo,socio_persona_id) values('1','78945',1);
-insert into socios (socio_id,socio_codigo,socio_persona_id) values('2','35678',2);
-insert into socios (socio_id,socio_codigo,socio_persona_id) values('3','78456',3);
-insert into socios (socio_id,socio_codigo,socio_persona_id) values('4','89564',4);
-insert into socios (socio_id,socio_codigo,socio_persona_id) values('5','68456',5);
-insert into socios (socio_id,socio_codigo,socio_persona_id) values('6','39564',6);
-insert into socios (socio_id,socio_codigo,socio_persona_id) values('7','48456',7);
-insert into socios (socio_id,socio_codigo,socio_persona_id) values('8','59564',8);
-insert into socios (socio_id,socio_codigo,socio_persona_id) values('9','19564',9);
-
--- --------------------------------------------------------
--- --------------------------------------------------------
 --
 -- Estructura de tabla para la tabla `clients`
 --
@@ -152,6 +133,188 @@ INSERT INTO `clients` (`id`, `usuario`, `nombre`, `sexo`, `nivel`, `email`, `tel
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `depositos`
+--
+
+CREATE TABLE `depositos` (
+  `depositos_id` int(11) NOT NULL,
+  `depositos_importe` decimal(10,2) NOT NULL,
+  `depositos_fecha` date NOT NULL,
+  `registro_padron_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `junta_directiva`
+--
+
+CREATE TABLE `junta_directiva` (
+  `junta_directiva_id` int(11) NOT NULL,
+  `periodo_id` int(11) NOT NULL,
+  `socio_id` int(11) NOT NULL,
+  `junta_directiva_cargo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `multa`
+--
+
+CREATE TABLE `multa` (
+  `multa_id` int(11) NOT NULL,
+  `multa_asistencia_id` int(11) NOT NULL,
+  `multa_importe` decimal(10,2) NOT NULL,
+  `multa_importe_1` decimal(10,2) NOT NULL,
+  `multa_tipo` varchar(30) NOT NULL,
+  `multa_fecha_cancelacion` date DEFAULT NULL,
+  `multa_estado` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `periodo`
+--
+
+CREATE TABLE `periodo` (
+  `periodo_id` int(11) NOT NULL,
+  `periodo_inicio` date NOT NULL,
+  `periodo_fin` date NOT NULL,
+  `periodo_descripcion` varchar(200) NOT NULL,
+  `periodo_estado` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `persona_id` int(11) NOT NULL,
+  `persona_nombres` varchar(30) NOT NULL,
+  `persona_ap_materno` varchar(30) NOT NULL,
+  `persona_ap_paterno` varchar(30) NOT NULL,
+  `persona_estado_civil` varchar(30) NOT NULL,
+  `persona_ocupacion_profesion` varchar(30) NOT NULL,
+  `persona_fecha_nacimiento` date NOT NULL,
+  `persona_dni` varchar(30) NOT NULL,
+  `persona_direccion` varchar(60) NOT NULL,
+  `persona_telefono` varchar(20) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`persona_id`, `persona_nombres`, `persona_ap_materno`, `persona_ap_paterno`, `persona_estado_civil`, `persona_ocupacion_profesion`, `persona_fecha_nacimiento`, `persona_dni`, `persona_direccion`, `persona_telefono`, `id_usuario`) VALUES
+(29, 'Deyvis Ronald', 'Cercado', 'Garcia', 's', 'ingeniero', '0000-00-00', '11111', '', '333333', 26),
+(30, 'Deyvis Ronald', 'Cercado', 'Garcia', 's', 'ingeniero', '0000-00-00', '48762828', '', '12345678', 27),
+(31, 'kenny misael', 'inciso', 'sss', 's', 'sss', '0000-00-00', '12312313', '', '+51988789159', 28),
+(32, 'kenny misael', 'inciso', 'assadasd', 'v', 'aaasdasd', '0000-00-00', '1231244123', '', '+51988789159', 29),
+(33, 'marcol', 'inciso', 'qqqq', 'c', 'qqq', '0000-00-00', '12313', '', '+51988789159', 30),
+(34, 'marcol', 'inciso', 'qqqq', 'c', 'qqq', '0000-00-00', '12313', '', '+51988789159', 31);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro_padron`
+--
+
+CREATE TABLE `registro_padron` (
+  `registro_padron_id` int(11) NOT NULL,
+  `registro_padron_periodo_id` int(11) NOT NULL,
+  `registro_padron_fecha` date NOT NULL,
+  `registro_padron_descripcion` varchar(300) NOT NULL,
+  `stand_id` int(11) NOT NULL,
+  `socio_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `registro_padron`
+--
+
+INSERT INTO `registro_padron` (`registro_padron_id`, `registro_padron_periodo_id`, `registro_padron_fecha`, `registro_padron_descripcion`, `stand_id`, `socio_id`) VALUES
+(1, 1, '0000-00-00', 'pueste de ventas', 1, 1),
+(2, 2, '0000-00-00', 'comestibles', 2, 2),
+(3, 3, '0000-00-00', 'servicios aseos', 3, 3),
+(4, 4, '0000-00-00', 'verduras', 4, 4),
+(5, 5, '0000-00-00', 'papas amarrillas', 5, 5),
+(6, 6, '0000-00-00', 'camotes asados', 6, 6),
+(7, 7, '0000-00-00', 'paltas verdes', 7, 7),
+(8, 8, '0000-00-00', 'naranajas azules', 8, 8),
+(9, 9, '0000-00-00', 'verduras verdes', 9, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id_rol` int(11) NOT NULL,
+  `rol_nombre` varchar(50) NOT NULL,
+  `rol_estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`id_rol`, `rol_nombre`, `rol_estado`) VALUES
+(1, 'Admin', 1),
+(2, 'Usuario', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `socios`
+--
+
+CREATE TABLE `socios` (
+  `socio_id` int(11) NOT NULL,
+  `socio_codigo` varchar(30) NOT NULL,
+  `socio_persona_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `socios`
+--
+
+INSERT INTO `socios` (`socio_id`, `socio_codigo`, `socio_persona_id`) VALUES
+(1, '78945', 1),
+(2, '35678', 2),
+(3, '78456', 3),
+(4, '89564', 4),
+(5, '68456', 5),
+(6, '39564', 6),
+(7, '48456', 7),
+(8, '59564', 8),
+(9, '195641', 91),
+(10, 'sss', 0),
+(11, 'ww', 0),
+(12, '1', 0),
+(13, 'kenny', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `stand`
+--
+
+CREATE TABLE `stand` (
+  `stand_id` int(11) NOT NULL,
+  `stand_fila` varchar(10) NOT NULL,
+  `stand_categoria` varchar(30) NOT NULL,
+  `stand_numero` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `users`
 --
 
@@ -159,19 +322,47 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `nombre` varchar(60) NOT NULL,
   `email` varchar(60) NOT NULL,
-  `password` varchar(60) NOT NULL
+  `password` varchar(60) NOT NULL,
+  `id_rol` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `nombre`, `email`, `password`) VALUES
-(1, 'Angel Rosendo Condori Coaquira', 'angel.condori@gmail.com', '123');
+INSERT INTO `users` (`id`, `nombre`, `email`, `password`, `id_rol`) VALUES
+(1, 'Angel Rosendo Condori Coaquira', 'angel.condori@gmail.com', '123', 0),
+(2, 'kenny misael inciso inciso', 'kennyinciso@upeu.edu.pe', '123', 0),
+(3, 'jork cardios zarse', 'cardios', '12345', 0),
+(4, 'crasla incisoa incisoa', 'craslaincisoa@upeu.edu', '123456', 0),
+(5, 'jorcus cersus jarcas', '@upeu.edu', '456789', 0),
+(6, 'kersd melsk hesto', '@upeu.edu', '789654', 0),
+(8, 'hysge palti zroso', '@upeu.edu', '987654', 0),
+(17, 'jork cardios zarse', 'cardios', '12345', 0),
+(18, 'crasla incisoa incisoa', 'craslaincisoa@upeu.edu', '123456', 0),
+(19, 'jorcus cersus jarcas', '@upeu.edu', '456789', 0),
+(20, 'kersd melsk hesto', '@upeu.edu', '789654', 0),
+(21, 'hayrs most salfas', '@upeu.edu', '321654', 0),
+(22, 'hysge palti zroso', '@upeu.edu', '987654', 0),
+(23, 'sasasasa', 'sasas', 'sasasa', 2),
+(24, 'Deyvia Ronald', 'deyvis', '12345', 1),
+(25, 'jose lusi ', 'jose', '123456', 2),
+(26, 'Deyvis Ronald', 'deyvis', '12345', 1),
+(27, 'Deyvis Ronald', 'Roand@gmail.com', '12345', 1),
+(28, 'kenny misael', 'aaas', 'sss', 1),
+(29, 'kenny misael', '123123', 'sadsasdasd', 1),
+(30, 'marcol', '', '', 0),
+(31, 'marcol', '', '', 0);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `actividades`
+--
+ALTER TABLE `actividades`
+  ADD PRIMARY KEY (`actividad_id`);
 
 --
 -- Indices de la tabla `books`
@@ -186,10 +377,10 @@ ALTER TABLE `clients`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `users`
+-- Indices de la tabla `periodo`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `periodo`
+  ADD PRIMARY KEY (`periodo_id`);
 
 --
 -- Indices de la tabla `persona`
@@ -198,21 +389,44 @@ ALTER TABLE `persona`
   ADD PRIMARY KEY (`persona_id`);
 
 --
+-- Indices de la tabla `rol`
 --
--- Indices de la tabla `persona`
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `socios`
 --
 ALTER TABLE `socios`
   ADD PRIMARY KEY (`socio_id`);
+
+--
+-- Indices de la tabla `stand`
+--
+ALTER TABLE `stand`
+  ADD PRIMARY KEY (`stand_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `actividades`
+--
+ALTER TABLE `actividades`
+  MODIFY `actividad_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de la tabla `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `clients`
@@ -221,22 +435,40 @@ ALTER TABLE `clients`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
--- AUTO_INCREMENT de la tabla `users`
+-- AUTO_INCREMENT de la tabla `periodo`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
+ALTER TABLE `periodo`
+  MODIFY `periodo_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `persona_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-COMMIT;
+  MODIFY `persona_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `socios`
 --
 ALTER TABLE `socios`
-  MODIFY `socio_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `socio_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `stand`
+--
+ALTER TABLE `stand`
+  MODIFY `stand_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
